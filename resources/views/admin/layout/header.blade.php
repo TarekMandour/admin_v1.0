613af583d9e9f132149326f991839a346894d82e
hmikki <30619974+hmikki@users.noreply.github.com>
2023-10-15 22:13:41 +0300

    u

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -41,7 +41,10 @@
         <!--begin::Navbar-->
         <div class="app-navbar flex-grow-1 justify-content-end" id="kt_app_header_navbar">
 
-
+<div class="btn btn-icon btn-custom btn-color-gray-600 btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative" id="kt_drawer_chat_toggle">
+                                    <i class="fonticon-alarm fs-2"></i>
+                                    <span class="position-absolute top-0 start-100 translate-middle badge badge-circle badge-danger w-15px h-15px ms-n4 mt-3">5</span>
+                                </div>
             <!--begin::User menu-->
             <div class="app-navbar-item ms-2 ms-lg-6" id="kt_header_user_menu_toggle">
                 <!--begin::Menu wrapper-->
